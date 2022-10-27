f4abc69cdc95052e910777ad83853a9aa8be41c3
Charafeddine Mechalikh <46229052+CharafeddineMechalikh@users.noreply.github.com>
2022-10-27 13:51:36 +0100

    Update SECRETS.py

--- src/SECRETS.py
+++ src/SECRETS.py
@@ -2,6 +2,5 @@ import os
 db_username = os.environ.get('DB_USER', 'alessandro')
 db_password = os.environ.get('DB_PSW', 'FogDirector')
 db_host = os.environ.get('DB_HOST', 'localhost') 
-#db_host = "server.apagiaro.it"
 db_port = int(os.environ.get('DB_PORT', 27017))
 
